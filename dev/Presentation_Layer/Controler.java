086788f35df2d83bcc9a061bffb3d0bdd93591de
reut <reutlevy30@gmail.com>
2020-04-09 19:14:10 +0300

    Add controler

---
+++ dev/Presentation_Layer/Controler.java
@@ -0,0 +1,14 @@
+package Presentation_Layer;
+
+public class Controler {
+
+    public void Add_driver(){
+
+    }
+    public void Show_drivers_List(){
+
+    }
+    public void Remove_driver(){
+
+    }
+}
