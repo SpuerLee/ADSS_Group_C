6730ed5e288f27bcf3e7e1e22ecbb9857ca773ed
reut <reutlevy30@gmail.com>
2020-04-09 19:25:30 +0300

    Change Buisness Layer

---
+++ dev/Presentation_Layer/Main.java
@@ -0,0 +1,98 @@
+package Presentation_Layer;
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Controler controler=new Controler();
+        System.out.println("Hello, please choose your option");
+        String[] A = new String[]{"Drivers", "Transportations", "Sites", "Suppliers", "Quit"};
+        String choice = null;
+        for (int i = 0; i < A.length; i++) {
+            System.out.println(i+1 + ". " + A[i]);
+        }
+        Scanner scan = new Scanner(System.in);
+        do {
+            choice = scan.nextLine();
+          switch (choice) {
+                case "1":{
+                    String[] drivers = new String[]{"Add new driver", "Show drivers list", "Remove driver"};
+                    String driver_option = null;
+                    for (int i = 0; i < drivers.length; i++) {
+                        System.out.println(i+1 + ". " + drivers[i]);
+                    }
+                    driver_option=scan.nextLine();
+                    switch (driver_option){
+                        case "1":{
+                           controler.Add_driver();
+                        }
+                        case "2":{
+                            controler.Show_drivers_List();
+                        }
+                        case "3":{
+                            controler.Remove_driver();
+                        }
+                    }
+                }
+              case "2": {
+                  String[] transports = new String[]{"Add new transport", "Show transports List", "Remove transport"};
+                  String option = null;
+                  for (int i = 0; i < transports.length; i++) {
+                      System.out.println(i+1 + ". " + transports[i]);
+                  }
+                  option = scan.nextLine();
+                  switch (option) {
+                      case "1": {
+                          controler.Add_transport();
+                      }
+                      case "2": {
+                          controler.Show_transports();
+                      }
+                      case "3": {
+                          controler.Remove_transport();
+                      }
+                  }
+              }
+              case "3": {
+                  String[] sites = new String[]{"Add new sites", "Show sites List", "Remove site"};
+                  String option = null;
+                  for (int i = 0; i < sites.length; i++) {
+                      System.out.println(i+1 + ". " + sites[i]);
+                  }
+                  option = scan.nextLine();
+                  switch (option) {
+                      case "1": {
+                          controler.Add_site();
+                      }
+                      case "2": {
+                          controler.Show_sites();
+                      }
+                      case "3": {
+                          controler.Remove_site();
+                      }
+                  }
+              }
+              case "4": {
+                  String[] suppliers = new String[]{"Add new supplier", "Show suppliers List", "Remove supplier"};
+                  String option = null;
+                  for (int i = 0; i < suppliers.length; i++) {
+                      System.out.println(i+1 + ". " + suppliers[i]);
+                  }
+                  option = scan.nextLine();
+                  switch (option) {
+                      case "1": {
+                          controler.Add_supplier();
+                      }
+                      case "2": {
+                          controler.Show_suppliers();
+                      }
+                      case "3": {
+                          controler.Remove_supplier();
+                      }
+                  }
+              }
+            }
+        } while (!choice.equals("5"));
+    }
+}
