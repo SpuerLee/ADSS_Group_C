9e620e58f86338d012287f9c66f6e2bae9e29aa3
reut <reutlevy30@gmail.com>
2020-05-20 01:13:29 +0300

    add Db

--- dev/Business_Layer/Modules/Address.java
+++ dev/Business_Layer/Modules/Address.java
@@ -2,13 +2,63 @@ package Business_Layer.Modules;
 
 public class Address {
 
+    private static int idcounter=1;
+    private int id;
     private String city;
     private String street;
     private int number;
+
+    public Address(int id, String city, String street,int number){
+        this.id=id;
+        this.city=city;
+        this.street=street;
+        this.number=number;
+    }
 
     public Address(String city, String street,int number){
         this.city=city;
         this.street=street;
         this.number=number;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public static int getIdCounter(){
+        return idcounter;
+    }
+    public static void setIdCounter(int id){
+        idcounter = id;
+    }
+
+
 }
